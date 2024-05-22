f117ed07a8616e1b26179a4294648ca038082bab
PitriFerra <pitriferra@gmail.com>
2024-05-22 12:53:43 +0200

    Got rid of the GPT version (copied in the other file)
    As title says

--- contracts/NFTime_single_brand.sol
+++ contracts/NFTime_single_brand.sol
@@ -1,53 +1,46 @@
-// SPDX-License-Identifier: UNLICENSED 
-// MyContract.sol
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-// Aspettare di usarli prima di mettere: Ownable
-contract NFTime is ERC721, ERC721URIStorage{
-    // Nessun guadagno tramite la blockchain. Tutto offchain
+contract NFTime is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-    // TODO:
-    // OK Logica minting e trasferimento NFTs
-    // Capire se tenere i metodi public o farli external (io li farei external se non hanno chiamate interne perché così andiamo a risparmiare gas)
-    // Capire se tenere i modifier o meno (io direi solo se usati molte volte nello smart contract)
-    // Gestione dei ruoli (come viene inserito un brand piuttosto che come viene "eletto" un certificatore):
-        // WAIT Funzione per aggiungere un certificatore (es. i brand possono aggiungerli -> penso alla logica (vedere cosa dicono Simo e Mario))
-    // Capire come vengono gestiti gli NFT nei progetti gia esistenti e di successo
+    // Address to receive the 5% commission
+    address private commissionRecipient;
 
-
-    // ------------
-    // Data Structs
-    // ------------
-
-    struct CertifierData
-    {
+    struct CertifierData {
         string name;
         bool isActive;
     }
 
+    struct SaleInfo {
+        uint256 price;
+        address seller;
+    }
 
-    // --------------------------
-    // Global / private variables
-    // --------------------------
-    
-    // Contract owner address
-    address private contractOwner;
     // List of certifiers
     mapping(address => CertifierData) private certifiers;
-    // Counter for NFT IDs
-    uint256 private _tokenIds;
-    
 
-    // --------------------
-    // constructor
-    // --------------------
-    
-    constructor(string memory brandName, string memory collectionName, string memory symbol) ERC721(collectionName, symbol)
-    { 
-        contractOwner = msg.sender;
+    // Mapping of token ID to sale info
+    mapping(uint256 => SaleInfo) public tokenSales;
+
+    // Events
+    event CertifierAdded(address indexed certifierAddress, string certifierName);
+    event CertifierRemoved(address indexed certifierAddress);
+    event ChangedCommissionRecipient(address indexed oldCommissionRecipient, address indexed newCommissionReceiver);
+    event NFTMinted(address indexed recipient, uint256 indexed tokenId, string tokenURI);
+    event NFTListedForSale(address indexed seller, uint256 indexed tokenId, uint256 price);
+    event NFTSold(address indexed seller, address indexed buyer, uint256 indexed tokenId, uint256 salePrice);
+
+    constructor(address NFTimeAddress, string memory brandName, string memory collectionName, string memory symbol) 
+        ERC721(collectionName, symbol) {
+        transferOwnership(msg.sender);
+        commissionRecipient = NFTimeAddress; // Set recipient to be NFTime address
         addCertifier(msg.sender, brandName); // Brand address is set as certifier as well so that it can mint too
     }
 
@@ -55,18 +48,23 @@ contract NFTime is ERC721, ERC721URIStorage{
     // --------------------
     // Methods - only owner
     // --------------------
-    
-    addCertifier(address certifierAddress, string memory certifierName) public
-    {
-        require(msg.sender == contractOwner, "Only the contract owner can perform this operation");
+
+    function addCertifier(address certifierAddress, string memory certifierName) public onlyOwner {
         certifiers[certifierAddress].name = certifierName;
         certifiers[certifierAddress].isActive = true;
+        emit CertifierAdded(certifierAddress, certifierName);
     }
 
-    removeCertifier(address certifierAddress) external
-    {
-        require(msg.sender == contractOwner, "Only the contract owner can perform this operation");
-        certifiers[certifierAddress].isActive = false;
+    function removeCertifier(address certifierAddress) external onlyOwner {
+        delete certifiers[certifierAddress];
+        emit CertifierRemoved(certifierAddress);
+    }
+
+    function setCommissionRecipient(address recipient) external {
+        require(msg.sender == commissionRecipient, "Only the commission recipient can transfer this role");
+        address oldCommissionRecipient = commissionRecipient;
+        commissionRecipient = recipient;
+        emit ChangedCommissionRecipient(oldCommissionRecipient, recipient);
     }
 
 
@@ -76,10 +74,12 @@ contract NFTime is ERC721, ERC721URIStorage{
 
     function mintNFT(address recipient, string memory tokenURI) external returns (uint256) {
         require(certifiers[msg.sender].isActive, "Only active 'certifiers' can perform this operation");
-        _tokenIds++;
-        _mint(recipient, _tokenIds);
-        _setTokenURI(_tokenIds, tokenURI);
-        return _tokenIds;
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        emit NFTMinted(recipient, newItemId, tokenURI);
+        return newItemId;
     }
 
 
@@ -87,8 +87,55 @@ contract NFTime is ERC721, ERC721URIStorage{
     // Methods - everyone
     // ------------------
 
-    function transferFrom(address _from, address _to, uint256 _tokenId) public override {
-        require(msg.sender == _from, "You can only transfer your own token");
-        super.transferFrom(_from, _to, _tokenId);
+    function listNFTForSale(uint256 tokenId, uint256 price) external {
+        address seller = msg.sender;
+        require(ownerOf(tokenId) == seller, "You can only list your own token");
+        require(price > 0, "Price must be greater than zero");
+
+        tokenSales[tokenId] = SaleInfo(price, seller);
+
+        emit NFTListedForSale(seller, tokenId, price);
+    }
+
+    function buyNFT(uint256 tokenId) external payable {
+        SaleInfo memory sale = tokenSales[tokenId];
+        require(sale.price > 0, "This token is not for sale");
+        require(msg.value == sale.price, "Incorrect sale price sent");
+
+        address seller = sale.seller;
+        uint256 salePrice = sale.price;
+
+        // Calculate commission
+        uint256 commission = salePrice / 100 * 5;
+        uint256 sellerProceeds = salePrice - commission;
+
+        // Handle payment
+        payable(commissionRecipient).transfer(commission);
+        payable(seller).transfer(sellerProceeds);
+
+        // Transfer the token to the buyer
+        _transfer(seller, msg.sender, tokenId);
+
+        // Clean up the sale info
+        delete tokenSales[tokenId];
+
+        emit NFTSold(seller, msg.sender, tokenId, salePrice);
+    }
+
+
+    // ---------------------
+    // Overrides (necessary)
+    // ---------------------
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
