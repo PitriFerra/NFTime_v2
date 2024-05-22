59ed2931e0b2d4026501675c7fbf4ca70fb46976
PitriFerra <pitriferra@gmail.com>
2024-05-22 12:51:33 +0200

    Improved NFT selling and payment logic
    Changed the old function to sell the NFT because it wasn't respecting the way we intended the payment to be performed. Now it's closer to the final idea

--- contracts/GPTime_single_brand.sol
+++ contracts/GPTime_single_brand.sol
@@ -18,19 +18,27 @@ contract NFTime is ERC721, ERC721URIStorage, Ownable {
         bool isActive;
     }
 
+    struct SaleInfo {
+        uint256 price;
+        address seller;
+    }
+
     // List of certifiers
     mapping(address => CertifierData) private certifiers;
+
+    // Mapping of token ID to sale info
+    mapping(uint256 => SaleInfo) public tokenSales;
 
     // Events
     event CertifierAdded(address indexed certifierAddress, string certifierName);
     event CertifierRemoved(address indexed certifierAddress);
     event ChangedCommissionRecipient(address indexed oldCommissionRecipient, address indexed newCommissionReceiver);
     event NFTMinted(address indexed recipient, uint256 indexed tokenId, string tokenURI);
+    event NFTListedForSale(address indexed seller, uint256 indexed tokenId, uint256 price);
     event NFTSold(address indexed seller, address indexed buyer, uint256 indexed tokenId, uint256 salePrice);
 
     constructor(address NFTimeAddress, string memory brandName, string memory collectionName, string memory symbol) 
-        ERC721(collectionName, symbol) 
-    {
+        ERC721(collectionName, symbol) {
         transferOwnership(msg.sender);
         commissionRecipient = NFTimeAddress; // Set recipient to be NFTime address
         addCertifier(msg.sender, brandName); // Brand address is set as certifier as well so that it can mint too
@@ -48,7 +56,7 @@ contract NFTime is ERC721, ERC721URIStorage, Ownable {
     }
 
     function removeCertifier(address certifierAddress) external onlyOwner {
-        certifiers[certifierAddress].isActive = false;
+        delete certifiers[certifierAddress];
         emit CertifierRemoved(certifierAddress);
     }
 
@@ -79,13 +87,26 @@ contract NFTime is ERC721, ERC721URIStorage, Ownable {
     // Methods - everyone
     // ------------------
 
-    // To resolve the fact that the actual payer is the smart contract itself
-    function sellNFT(address buyer, uint256 tokenId, uint256 salePrice) external {
+    function listNFTForSale(uint256 tokenId, uint256 price) external {
         address seller = msg.sender;
-        require(ownerOf(tokenId) == seller, "You can only sell your own token");
+        require(ownerOf(tokenId) == seller, "You can only list your own token");
+        require(price > 0, "Price must be greater than zero");
+
+        tokenSales[tokenId] = SaleInfo(price, seller);
+
+        emit NFTListedForSale(seller, tokenId, price);
+    }
+
+    function buyNFT(uint256 tokenId) external payable {
+        SaleInfo memory sale = tokenSales[tokenId];
+        require(sale.price > 0, "This token is not for sale");
+        require(msg.value == sale.price, "Incorrect sale price sent");
+
+        address seller = sale.seller;
+        uint256 salePrice = sale.price;
 
         // Calculate commission
-        uint256 commission = (salePrice * 5) / 100;
+        uint256 commission = salePrice / 100 * 5;
         uint256 sellerProceeds = salePrice - commission;
 
         // Handle payment
@@ -93,9 +114,12 @@ contract NFTime is ERC721, ERC721URIStorage, Ownable {
         payable(seller).transfer(sellerProceeds);
 
         // Transfer the token to the buyer
-        _transfer(seller, buyer, tokenId);
+        _transfer(seller, msg.sender, tokenId);
 
-        emit NFTSold(seller, buyer, tokenId, salePrice);
+        // Clean up the sale info
+        delete tokenSales[tokenId];
+
+        emit NFTSold(seller, msg.sender, tokenId, salePrice);
     }
 
 
