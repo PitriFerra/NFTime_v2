2552f06c086c6af745a1651efe50f2351e7acbdf
PitriFerra <pitriferra@gmail.com>
2024-05-28 10:32:34 +0200

    Trying inserting roles (not tested)
    Insertion of ADMIN, PAUSER and MINTER roles (BURNER to add)

---
+++ contracts/NFTime_from_Remix_IDE.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+
+
+// TODO:
+// - (?) Figura admin per bloccare tutto e farsi pagare da brand;
+// - Pagamento per minting tramite oracolo + penso a logica di upgrade se necessaria;
+// - OK Sostituire buy con transfer;
+// - Valutare inserimento burn tra le features (i.e. per certificatori "corrotti") -> memorizzazione dell'associazione <certificatori, NFT mintati>;
+    // - Salvataggio tokenID degli NFT;
+// - Metodo che ritorna la lista dei certificatori;
+// - (?) Implementare uso corretto dei ruoli;
+
+
+contract MyToken is ERC721, ERC721Pausable, ERC721URIStorage, AccessControl {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // List of certifiers
+    mapping(address => string) private certifiers;
+    // Address to receive the commission
+    address private commissionRecipient;
+
+    // Roles
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    // Events
+    event CertifierAdded(address indexed certifierAddress, string certifierName);
+    event CertifierRemoved(address indexed certifierAddress, string certifierName);
+    event ChangedCommissionRecipient(address indexed oldCommissionRecipient, address indexed newCommissionReceiver);
+    
+    constructor(address initialOwner)
+        ERC721("Rolex_NFTime", "WTC")
+    {
+        _grantRole(ADMIN_ROLE, initialOwner);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _setRoleAdmin(MINTER_ROLE, ADMIN_ROLE);
+        _setRoleAdmin(PAUSER_ROLE, PAUSER_ROLE);
+    }
+
+
+    // --------------------
+    // Methods - only owner
+    // --------------------
+
+    function addCertifier(address certifierAddress, string memory certifierName) 
+        external 
+        onlyRole(ADMIN_ROLE) 
+    {
+        grantRole(MINTER_ROLE, certifierAddress);
+        emit CertifierAdded(certifierAddress, certifierName);
+    }
+
+    function removeCertifier(address certifierAddress) 
+        external 
+        onlyRole(ADMIN_ROLE)
+    {
+        revokeRole(MINTER_ROLE, certifierAddress);
+        emit CertifierRemoved(certifierAddress, certifiers[certifierAddress]);
+    }
+
+
+    // -----------------------------------
+    // Methods - only commission recipient
+    // -----------------------------------
+
+    function setCommissionRecipient(address recipient) external onlyRole(PAUSER_ROLE) {
+        commissionRecipient = recipient;
+        grantRole(PAUSER_ROLE, recipient);
+        renounceRole(PAUSER_ROLE, msg.sender);
+        emit ChangedCommissionRecipient(msg.sender, recipient);
+    }
+
+    function pause() external onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() external onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+
+    // -------------------------
+    // Methods - only certifiers
+    // -------------------------
+
+    function getCommissionValue() pure internal returns (uint256) {
+        return 10;
+    }
+
+    function safeMint(address to, string memory token_URI) 
+        external 
+        payable 
+        onlyRole(MINTER_ROLE) 
+        returns (uint256) 
+    {
+        uint256 commissionValue = getCommissionValue();
+        require(msg.value >= commissionValue, "Not enough commission sent");
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(to, newItemId);
+        _setTokenURI(newItemId, token_URI);
+        payable(commissionRecipient).transfer(commissionValue);
+        return newItemId;
+    }
+
+
+    // ---------------------
+    // Overrides (necessary)
+    // ---------------------
+
+    function tokenURI(uint256 tokenId) 
+        public 
+        view 
+        override(ERC721, ERC721URIStorage) 
+        returns (string memory) 
+    {
+        return super.tokenURI(tokenId);
+    }
+    
+    function supportsInterface(bytes4 interfaceId) 
+        public 
+        view 
+        override(ERC721, ERC721URIStorage, AccessControl) 
+        returns (bool) 
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Pausable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+}
--- contracts/NFTime_single_brand.sol
+++ contracts/NFTime_single_brand.sol
@@ -12,6 +12,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 // - Sostituire buy con transfer;
 // - Valutare inserimento burn tra le features (i.e. per certificatori "corrotti") -> memorizzazione dell'associazione <certificatori, NFT mintati>;
     // - Salvataggio tokenID degli NFT;
+// - Metodo che ritorna la lista dei certificatori;
 
 contract NFTime is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
@@ -33,17 +34,12 @@ contract NFTime is ERC721, ERC721URIStorage, Ownable {
     // List of certifiers
     mapping(address => CertifierData) private certifiers;
 
-    // Mapping of token ID to sale info
-    mapping(uint256 => SaleInfo) public tokenSales;
-
     // Events
     event CertifierAdded(address indexed certifierAddress, string certifierName);
     event CertifierRemoved(address indexed certifierAddress);
     event ChangedCommissionRecipient(address indexed oldCommissionRecipient, address indexed newCommissionReceiver);
     event NFTMinted(address indexed recipient, uint256 indexed tokenId, string tokenURI);
-    event NFTListedForSale(address indexed seller, uint256 indexed tokenId, uint256 price);
-    event NFTSold(address indexed seller, address indexed buyer, uint256 indexed tokenId, uint256 salePrice);
-
+    
     constructor(address brandAddress, string memory brandName, string memory collectionName, string memory symbol) 
         ERC721(collectionName, symbol) {
         transferOwnership(brandAddress);
@@ -106,40 +102,7 @@ contract NFTime is ERC721, ERC721URIStorage, Ownable {
     // Methods - everyone
     // ------------------
 
-    function listNFTForSale(uint256 tokenId, uint256 price) external {
-        address seller = msg.sender;
-        require(ownerOf(tokenId) == seller, "You can only list your own token");
-        require(price > 0, "Price must be greater than zero");
 
-        tokenSales[tokenId] = SaleInfo(price, seller);
-
-        emit NFTListedForSale(seller, tokenId, price);
-    }
-
-    function buyNFT(uint256 tokenId) external payable {
-        SaleInfo memory sale = tokenSales[tokenId];
-        require(sale.price > 0, "This token is not for sale");
-        require(msg.value == sale.price, "Incorrect sale price sent");
-
-        address seller = sale.seller;
-        uint256 salePrice = sale.price;
-
-        // Calculate commission
-        uint256 commission = salePrice / 100 * 5;
-        uint256 sellerProceeds = salePrice - commission;
-
-        // Handle payment
-        payable(commissionRecipient).transfer(commission);
-        payable(seller).transfer(sellerProceeds);
-
-        // Transfer the token to the buyer
-        _transfer(seller, msg.sender, tokenId);
-
-        // Clean up the sale info
-        delete tokenSales[tokenId];
-
-        emit NFTSold(seller, msg.sender, tokenId, salePrice);
-    }
 
 
     // ---------------------
